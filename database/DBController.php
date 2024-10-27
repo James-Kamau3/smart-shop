86e491d74dc2f31b240b541280d0834588943cda
James <kamaujames3705@gmail.com>
2024-10-27 09:42:38 +0300

    secured credentials

--- database/DBController.php
+++ database/DBController.php
@@ -1,24 +1,27 @@
 <?php
 
+
 class DBController
 {
 
     //properties
-    // protected $host = '	sql200.infinityfree.com';
-    // protected $user = 'if0_37044196';
-    // protected $password = 'Storenow100';
-    // protected $database = 'if0_37044196_ecomm';
-    // public $con = null;
-    
-    protected $host = 'localhost';
-    protected $user = 'root';
-    protected $password = '';
-    protected $database = 'ecomm_store';
+    protected $host;
+    protected $user;
+    protected $password;
+    protected $database;
     public $con = null;
 
     //constructor to open connection
     public function __construct()
     {
+        $config = include dirname(__DIR__, 1) . '/config.php';
+
+        $this->host = $config['DB_HOST'];
+        $this->user = $config['DB_USER'];
+        $this->password = $config['DB_PASSWORD'];
+        $this->database = $config['DB_NAME'];
+
+        //Initializing db connection
         $this->con = mysqli_connect($this->host, $this->user, $this->password, $this->database);
 
         if ($this->con->connect_error) {
